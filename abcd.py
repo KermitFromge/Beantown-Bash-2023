d4a8d0e6dc9db18a5c70edb0d6b36894defa980d
yoonchul s <yoonchulshin610@gmail.com>
2023-04-01 14:53:05 -0400

    iasjdhasd

--- abcd.py
+++ abcd.py
@@ -1,27 +1,23 @@
 import requests
 import pandas as pd
-import time
+import finnhub
+
+
 
 api_key = 'cgk5dkhr01qq3c3u0lf0cgk5dkhr01qq3c3u0lfg'
+
+finnhub_client = finnhub.Client(api_key=api_key)
 
 df = pd.read_csv("sp500.csv")
 
 tickers = df['Symbol'].tolist()
 
 def pull_sentiment(symbol):
-    url = f'https://finnhub.io/api/v1/stock/insider-sentiment?symbol={symbol}&from=2023-03-31&to=2023-04-01&token={api_key}'
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.json()
-
-        if 'data' in data:
-            for record in data['data']:
-                if record['symbol'] == symbol and record['year'] == 2023 and record['month'] == 3:
-                    mspr_value = record.get('mspr', None)
-                    if mspr_value is not None:
-                        return mspr_value
-    return None
+    count =  0
+    for i in range(50):
+        count += 1
+    sentiment_data = finnhub_client.stock_insider_sentiment(symbol, '2023-03-01', '2023-04-01') 
+    return(sentiment_data['data'][count]['mspr'])
 
 sentiment_values = []
 for symbol in tickers:
@@ -31,7 +27,5 @@ for symbol in tickers:
     else:
         print(f"Failed to get mspr for {symbol}")
 
-    # Add a delay between requests
-    time.sleep(1)  # Sleep for 1 second
 
 print(sentiment_values)
