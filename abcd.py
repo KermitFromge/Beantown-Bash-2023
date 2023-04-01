1ac9dcf94c99f3afa49d22eafa822c14bde3d2eb
yoonchul s <yoonchulshin610@gmail.com>
2023-04-01 13:32:32 -0400

    iasjdwqd

--- abcd.py
+++ abcd.py
@@ -1,31 +1,31 @@
 import requests
 import finnhub
-import csv
 import pandas as pd
 
-print("START")
-finnhub_client = finnhub.Client(api_key="cgk5dkhr01qq3c3u0lf0cgk5dkhr01qq3c3u0lfg")
+api_key = 'cgk5dkhr01qq3c3u0lf0cgk5dkhr01qq3c3u0lfg'
 
-df =  pd.read_csv("sp500.csv")
+finnhub_client = finnhub.Client(api_key=api_key)
 
-tickers = []
+df = pd.read_csv("sp500.csv")
 
-for i in df['Symbol']:
-    tickers.append(i)
-def fetch_insider_sentiment(ticker):
-        sentiment_data = finnhub_client.stock_insider_sentiment(ticker, '2023-03-01', '2023-03-31')
-        print(sentiment_data)
-        if 'mspr' in sentiment_data:
-            return sentiment_data['mspr']
-mspr_data = {}
-for i in range(2):
-    print("checkpoint")
-    print(tickers[i])
-    mspr = fetch_insider_sentiment(tickers[i])
-    print(mspr)
-    if mspr is not None:
-        mspr_data[tickers] = mspr
+tickers = df['Symbol'].tolist()
 
-print(mspr_data)
+def pull_sentiment(symbol):
+    try:
+            finnhub_client.stock_insider_sentiment(symbol, '2023-03-31', '2023-04-01')
+    except Exception as e:  
+        print(f"Error fetching data for {symbol}: {e}")
+        return None, None
 
-#print(finnhub_client.stock_insider_sentiment('AAPL', '2023-03-01', '2023-03-31'))
+
+sentiments = {}
+for symbol in tickers[:50]:
+    price, revenue = fetch_price_and_revenue(symbol)
+    if price and revenue:
+        ps_ratio = price / revenue
+        ps_ratios[symbol] = ps_ratio
+
+lowest_ps_ratio_stock = min(ps_ratios, key=ps_ratios.get)
+lowest_ps_ratio_value = ps_ratios[lowest_ps_ratio_stock]
+
+print(f'The stock with the lowest P/S ratio is {lowest_ps_ratio_stock} with a value of {lowest_ps_ratio_value}.')
