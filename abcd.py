8c7998e4b36f7c6f399f7a6cfc6be888028cb888
yoonchul s <yoonchulshin610@gmail.com>
2023-04-01 16:27:26 -0400

    final

--- abcd.py
+++ abcd.py
@@ -1,11 +1,8 @@
-import requests
 import pandas as pd
 import time 
 import finnhub
 
-
-
-api_key = 'cgk8au9r01qq3c3u2fk0cgk8au9r01qq3c3u2fkg'
+api_key = 'cgk5dkhr01qq3c3u0lf0cgk5dkhr01qq3c3u0lfg'
 
 finnhub_client = finnhub.Client(api_key=api_key)
 
@@ -14,9 +11,11 @@ df = pd.read_csv("sp500.csv")
 tickers = df['Symbol'].tolist()
 
 def pull_sentiment(symbol):
-    sentiment_data = finnhub_client.stock_insider_sentiment(symbol, '2023-03-31', '2023-04-01') 
-    print(sentiment_data)
-    return(sentiment_data.get('data')[0].get('mspr'))
+    sentiment_data = finnhub_client.stock_insider_sentiment(symbol, '2023-03-31', '2023-04-01')
+    if sentiment_data and 'data' in sentiment_data and len(sentiment_data['data']) > 0:
+        return [sentiment_data['data'][0].get('symbol'), sentiment_data['data'][0].get('mspr') ] 
+    else:
+        return None
 
 sentiment_values = []
 for symbol in tickers:
@@ -26,7 +25,7 @@ for symbol in tickers:
     else:
         print(f"Failed to get mspr for {symbol}")
 
-    time.sleep(2) 
-
+    time.sleep(1)
 
 print(sentiment_values)
+
